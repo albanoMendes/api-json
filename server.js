4d47be0165e1e5760d1ecf12797eca747e679456
overtimenl <overtimenl47@gmail.com>
2025-02-16 17:13:09 -0300

    Correção erro 500 no Vercel

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const jsonServer = require("json-server");
+
+console.log("Iniciando JSON Server...");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
@@ -10,8 +13,8 @@ const PORT = process.env.PORT || 3000;
 
 server
   .listen(PORT, () => {
-    console.log(`JSON Server está rodando na porta ${PORT}`);
+    console.log(`✅ JSON Server está rodando na porta ${PORT}`);
   })
   .on("error", (err) => {
-    console.error("Erro ao iniciar o servidor:", err);
+    console.error("❌ Erro ao iniciar o servidor:", err);
   });
